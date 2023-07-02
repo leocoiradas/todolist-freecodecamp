8e2cbdbb234bc0298af36626fe4fbb962f2dea9c
Alan Coiradas <coiradasalan@gmail.com>
2023-07-02 15:26:59 -0300

    se añadió un input que servirá para introducir un texto y el botón que creará una tarea con ese mismo texto

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import freeCodeCampLogo from './assets/freecodecamp-logo.png'
 import Task from './components/Task';
+import TaskForm from './components/TaskForm';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       </div>
       <div className='list-main'>
         <h1>My Tasks</h1>
+        <TaskForm />
         <Task text='Aprender React'/>
       </div>
     </div>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import '../components-styles/taskform.css'
 function TaskForm(){
     return(
         <form className="task-form">
             <input className="task-input" type="text" placeholder="Escribe una Tarea" name="text" />
+            <button className="task-button">Add Task</button>
         </form>
+        
     )
 }
 export default TaskForm
