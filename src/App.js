ab81d96707507aff41dc6c188fddfaf4d4549713
Alan Coiradas <coiradasalan@gmail.com>
2023-07-02 11:45:57 -0300

    se añadió el logo y se normalizaron los estilos de la app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import freeCodeCampLogo from './assets/freecodecamp-logo.png'
 function App() {
   return (
     <div className="App">
       <div className='logo-container'>
-        
+        <img src={freeCodeCampLogo} className='freecodecamp-logo' />
       </div>
       
     </div>
